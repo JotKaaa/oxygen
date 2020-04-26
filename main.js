029e9e6238758fc6debd3bd1499d5b27196840e8
Joanna Kotynska <joanna.kotynska@gmail.com>
2020-04-26 17:43:41 +0200

    main changes in the mobile version of the website done

--- main.js
+++ main.js
@@ -15,15 +15,61 @@ const indexTyping = setInterval(discriminationAddLetter, 100);
 
 // -------------------------------COLORFUL BG-----------------------------------------
 
-document.body.addEventListener("mousemove", function (event) {
-  const x = event.clientX + 1;
-  const y = event.clientY + 1;
-  const width = window.innerWidth;
-  const height = window.innerHeight;
+// document.body.addEventListener("mousemove", function (event) {
+//   const x = event.clientX + 1;
+//   const y = event.clientY + 1;
+//   const width = window.innerWidth;
+//   const height = window.innerHeight;
 
-  const red = (x / width) * 100;
-  const green = (y / height) * 100;
-  const blue = ((x / width) * 100 + (y / height) * 100) / 2;
+//   const red = (x / width) * 100;
+//   const green = (y / height) * 100;
+//   const blue = ((x / width) * 100 + (y / height) * 100) / 2;
 
-  document.body.style.backgroundColor = `rgba(${red}%, ${green}%, ${blue}%, 9%)`;
+//   document.body.style.backgroundColor = `rgba(${red}%, ${green}%, ${blue}%, 9%)`;
+// });
+
+// -----------------Menu mobile--------------------
+
+const burger = document.querySelector(".burger");
+
+burger.addEventListener("click", function () {
+  burger.classList.toggle("active");
+});
+
+// ---------------Nav Slider -----------------
+
+$(".aboutMobile").on("click", function () {
+  $("body, html").animate(
+    {
+      scrollTop: $(".s1").offset().top,
+    },
+    1000
+  );
+});
+
+$(".podcastsMobile").on("click", function () {
+  $("body, html").animate(
+    {
+      scrollTop: $(".s2").offset().top,
+    },
+    1000
+  );
+});
+
+$(".workshopsMobile").on("click", function () {
+  $("body, html").animate(
+    {
+      scrollTop: $(".s3").offset().top,
+    },
+    1000
+  );
+});
+
+$(".contactMobile").on("click", function () {
+  $("body, html").animate(
+    {
+      scrollTop: $(".s4").offset().top,
+    },
+    1000
+  );
 });
